79f9bfec87523ed193dac3b5971134ced30baf7d
azizIirawan <azizirawan_2226250114@mhs.mdp.ac.id>
2024-12-05 13:16:01 +0700

    sign up

--- lib/screen/sign_up_screen.dart
+++ lib/screen/sign_up_screen.dart
@@ -16,109 +16,129 @@ class _SignUpScreenState extends State<SignUpScreen> {
 
   String _errorText = '';
 
-  bool _isSignedIn = false;
   bool _obscurePassword = false;
+
+  void _signUp() {
+    String name = _nameController.text.trim();
+    String username = _usernameController.text.trim();
+    String password = _passwordController.text.trim();
+
+    if (password.length < 8 ||
+        !password.contains(RegExp(r'[A-Z]')) ||
+        !password.contains(RegExp(r'[a-z]')) ||
+        !password.contains(RegExp(r'[0-9]')) ||
+        !password.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]')))
+        
+    print(' *** sing up berhasil');
+    print('Name : $name');
+    print('Nama pengguna : $username');
+    print('password : $password');
+  }
+
+  @override
+  void dispose() {
+    // TODO : implemens dispose
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //TODO: 2. Pasang appbar
       appBar: AppBar(
         title: const Text('Sign Up'),
       ),
-      //TODO: 3. Pasang Body
       body: Center(
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(16),
             child: Form(
-                child: Column(
-              //TODO: 4. Atur MainAxisAlignment dan CrossAxisAlignment
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                //TODO: 9. Pasang TextFormField Nama
-                TextFormField(
-                  controller: _nameController,
-                  decoration: const InputDecoration(
-                    labelText: "ziz",
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                //TODO: 5. Pasang TextFormFIeld Nama Pengguna
-                TextFormField(
-                  controller: _usernameController,
-                  decoration: const InputDecoration(
-                    labelText: "Nama Pengguna",
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                //TODO: 6. Pasang TextFormField Kata Sandi
-                const SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  controller: _passwordController,
-                  decoration: InputDecoration(
-                    labelText: "Kata Sandi",
-                    errorText: _errorText.isNotEmpty ? _errorText : null,
-                    border: const OutlineInputBorder(),
-                    suffixIcon: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          _obscurePassword = !_obscurePassword;
-                        });
-                      },
-                      icon: Icon(_obscurePassword
-                          ? Icons.visibility_off
-                          : Icons.visibility),
+              child: Column(
+                //TODO: 4. Atur MainAxisAlignment dan CrossAxisAlignment
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  //TODO: 9. Pasang TextFormField Nama
+                  TextFormField(
+                    controller: _nameController,
+                    decoration: InputDecoration(
+                      labelText: "ziz",
+                      errorText: _errorText.isNotEmpty ? _errorText : null,
+                      border: const OutlineInputBorder(),
                     ),
                   ),
-                  obscureText: _obscurePassword,
-                ),
-                //TODO: 7. Pasanag ElevatedButton Sign Up
-                const SizedBox(
-                  height: 20,
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('Sign Up'),
-                ),
-                //TODO: 8. Pasang TextButton Sign Up
-                const SizedBox(
-                  height: 20,
-                ),
-                // TextButton(
-                //     onPressed: () {},
-                //     child: Text('Belum punya akun? Daftar disini')
-                // ),
-                RichText(
-                  text: TextSpan(
-                    text: 'Sudah punya akun?',
-                    style: const TextStyle(
-                      fontSize: 16, color: Colors.deepPurple,
+                  //TODO: 5. Pasang TextFormFIeld Nama Pengguna
+                  TextFormField(
+                    controller: _usernameController,
+                    decoration: const InputDecoration(
+                      labelText: "Nama Pengguna",
+                      border: OutlineInputBorder(),
                     ),
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: 'Masuk disini.',
-                        style: const TextStyle(
-                          color: Colors.blue,
-                          decoration: TextDecoration.underline,
-                          fontSize: 16,
-                        ),
-                        recognizer: TapGestureRecognizer()
-                        ..onTap = () {},
+                  ),
+                  //TODO: 6. Pasang TextFormField Kata Sandi
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      labelText: "Kata Sandi",
+                      errorText: _errorText.isNotEmpty ? _errorText : null,
+                      border: const OutlineInputBorder(),
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _obscurePassword = !_obscurePassword;
+                          });
+                        },
+                        icon: Icon(_obscurePassword
+                            ? Icons.visibility_off
+                            : Icons.visibility),
                       ),
-                    ],
+                    ),
+                    obscureText: _obscurePassword,
                   ),
-                )
-              ],
+                  //TODO: 7. Pasanag ElevatedButton Sign Up
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Sign Up'),
+                  ),
+                  //TODO: 8. Pasang TextButton Sign Up
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  // TextButton(
+                  //     onPressed: () {},
+                  //     child: Text('Belum punya akun? Daftar disini')
+                  // ),
+                  RichText(
+                    text: TextSpan(
+                      text: 'Sudah punya akun?',
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.deepPurple,
+                      ),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: 'Masuk disini.',
+                          style: const TextStyle(
+                            color: Colors.blue,
+                            decoration: TextDecoration.underline,
+                            fontSize: 16,
+                          ),
+                          recognizer: TapGestureRecognizer()..onTap = () {},
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
-                  ),
           ),
         ),
       ),
     );
   }
 }
-
